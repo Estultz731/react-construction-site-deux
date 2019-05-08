bcd40620e5c09f9629b8aa0dd3326fb964fb036f
Estultz731 <elizablue731@gmail.com>
2019-05-08 11:10:55 -0400

    added navbar and images folder

--- src/App.js
+++ src/App.js
@@ -2,24 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Nav = () => (<nav>
+  <a className="left" href="#site">Acme Construction</a>
+  <a className="right" href="#social">Social</a>
+  <a className="right" href="#contact">Contact</a>
+</nav>)
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+    </>
   );
 }
 
